543b69e4c2792971dfc62ba5df57898a91dcd78f
Mohamed Nur <mohamed.nur@codecrimson.com>
2019-11-17 21:35:51 -0500

    parse scopes into unique set

--- crimson_recon.py
+++ crimson_recon.py
@@ -1,29 +1,38 @@
 import json, sys , re
 from codecs import encode, decode
+from ast import literal_eval
+
+#def enumerate(input):
+	
+no_regex = str.maketrans("", "", "$^\\")
+scopeList = set()
 
 with open('scope.json') as json_file:
-    data = json.load(json_file)
-    scope = data['target']['scope']['include']
+	data = json.load(json_file)
+	scope = data['target']['scope']['include']
 
-    p = re.compile("[a-z]")
-    for s in scope:
-    	# Don't parse scopes that need to be created
-    	if(s['enabled'] == True and ("h1" not in s['host'] and "h1" not in s['file'])):
-    		#Remove escape characters and normalize scope regex
-    		matchHost = decode(encode(s['host'], 'latin-1', 'backslashreplace'), 'unicode-escape')
-    		matchFile = decode(encode(s['file'], 'latin-1', 'backslashreplace'), 'unicode-escape')
-    		matchFile = matchFile.replace(".*.*",".*")
-    		matchHost = matchHost.replace("$","")
-    		matchFile = matchFile.replace(".*.*",".*")
-    		matchFile = matchFile.replace("^/","\\") + "$"
-    		scopeRegex = matchHost+matchFile
-    		print (scopeRegex)
+	p = re.compile("[a-z]")
+	for s in scope:
+		# Don't parse scopes that need to be created
+		if(s['enabled'] == True and ("h1" not in s['host'] and "h1" not in s['file'])):
+			#Remove escape characters and normalize scope regex
+			matchHost = decode(encode(s['host'], 'latin-1', 'backslashreplace'), 'unicode-escape')
+			matchFile = decode(encode(s['file'], 'latin-1', 'backslashreplace'), 'unicode-escape')
+			matchFile = matchFile.replace(".*.*",".*").replace(".*.*",".*").replace("^/","\\/").replace("//","/") + "$"
+			matchHost = matchHost.replace("$","")
+			scopeRegex = matchHost+matchFile
+			#print (scopeRegex)
+			sanitized = scopeRegex.translate(no_regex)
+			sanitized = sanitized.replace(".*","<")
+			scopeList.add(s['protocol'] +"://" + sanitized)
 
-    		#Find number of wildcards in scope regex
-    		wildcardRegex = "\\.\\*"
-    		p = re.compile(wildcardRegex)
-    		for m in p.finditer(scopeRegex):
-    			print(m.start(), m.group())
-    		
-    		#sys.exit()
+#Find number of places to enumerate
+for s in scopeList:
+	print (s)
+	marker = "<"
+	p = re.compile(marker)
+	for m in p.finditer(s):
+		print(m.start(), m.group())
+
+	#sys.exit()
 
