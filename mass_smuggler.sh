68b07b97edacf3caaa72183e820f12938a5fb4e7
Mohamed Nur <mohamed.nur@codecrimson.com>
2020-03-19 14:04:58 -0400

    Minor change to delete previous smuggler test result file

--- mass_smuggler.sh
+++ mass_smuggler.sh
@@ -5,7 +5,9 @@ home=${!h}
 
 echo "Testing all subdomains for http request smuggling"
 outputFile=$(echo $1 | cut -d \. -f 1)
-echo $outputFile
+
+rm -f ${outputFile}_smuggler.txt
+
 while IFS= read -r line
 do
   $(python3 $home/tools/pentest-tools/smuggler.py -u https://$line/ -v 1 -t 10 >> ${outputFile}_smuggler.txt)
