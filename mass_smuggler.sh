2bc5c8dbe6b155dd7838334f1ffaa58f5ec9345a
Mohamed Nur <mohamed.nur@codecrimson.com>
2020-04-01 19:06:32 -0400

    Change request smuggler wrapped to use input file instead of line by line

--- mass_smuggler.sh
+++ mass_smuggler.sh
@@ -7,9 +7,10 @@ echo "Testing all subdomains for http request smuggling"
 outputFile=$(echo $1 | cut -d \. -f 1)
 
 rm -f ${outputFile}_smuggler.txt
+rm -f ${outputFile}_https.txt
 
-while IFS= read -r line
-do
-  $(python3 $home/tools/pentest-tools/smuggler.py -u https://$line/ -v 1 -t 10 >> ${outputFile}_smuggler.txt)
-done < "$input"
+sed 's/^/https:\/\//' ${input} > ${outputFile}_https.txt
+
+python3 $home/tools/pentest-tools/smuggler.py -u ${outputFile}_https.txt -v 1 -t 10 >> ${outputFile}_smuggler.txt
+
 
